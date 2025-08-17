1d3feca49bd6fdcd1a20603a160e81ca4bee52b7
angelSantori <antonyo.da.mm@gmail.com>
2025-08-17 15:08:40 -0700

    Mensaje de bienvenida

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
+import 'dart:math';
 import 'dart:typed_data';
 import 'package:image/image.dart' as img;
 import 'package:flutter/material.dart';
@@ -35,12 +36,35 @@ class _HomeScreenState extends State<HomeScreen> {
   img.Image? _capturedImage;
   bool _isCapturing = false;
   int _captureCount = 0;
+  bool _showWelcomeDialog = false;
+  String? _welcomeUserName;
+  String? _randomGifPath;
 
   @override
   void initState() {
     super.initState();
     _loadUsers();
     _checkBiometrics();
+  }
+
+  void _showWelcomeDialogEmergente(String userName) {
+    final random = Random();
+    final gifNumber =
+        random.nextInt(5) + 1; // Asume que tienes hola1.gif a hola5.gif
+    setState(() {
+      _welcomeUserName = userName;
+      _randomGifPath = 'assets/gifs/hola$gifNumber.gif';
+      _showWelcomeDialog = true;
+    });
+
+    // Cerrar automáticamente después de 3 segundos
+    Future.delayed(const Duration(seconds: 3), () {
+      if (mounted) {
+        setState(() {
+          _showWelcomeDialog = false;
+        });
+      }
+    });
   }
 
   Future<void> _checkBiometrics() async {
@@ -135,6 +159,7 @@ class _HomeScreenState extends State<HomeScreen> {
       final success = await _asistenciaController.addAsistencia(asistencia);
 
       if (success) {
+        _showWelcomeDialogEmergente(matchedUser.user_Name ?? 'Usuario');
         _showSnackBar(
           'Asistencia registrada para ${matchedUser.user_Name}. Similitud: ${(highestSimilarity * 100).toStringAsFixed(1)}%',
         );
@@ -313,136 +338,171 @@ class _HomeScreenState extends State<HomeScreen> {
     final size = MediaQuery.of(context).size;
 
     return Scaffold(
-      body: Container(
-        width: size.width,
-        height: size.height,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              Colors.white,
-              const Color.fromARGB(255, 96, 156, 217),
-              const Color.fromARGB(255, 4, 134, 240),
-            ],
-          ),
-        ),
-        child: Center(
-          child: SafeArea(
-            child: SingleChildScrollView(
-              padding: const EdgeInsets.symmetric(horizontal: 24),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    width: 200,
-                    child: ElevatedButton(
-                      onPressed: _captureAttendanceFace,
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.white,
-                        foregroundColor: Colors.blue.shade900,
-                        padding: const EdgeInsets.symmetric(vertical: 16),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        elevation: 0,
-                      ),
-                      child: const Text('Registrar Asistencia'),
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  DropdownButtonFormField<Users>(
-                    value: _selectedUser,
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12),
-                        borderSide: BorderSide.none,
-                      ),
-                      contentPadding: const EdgeInsets.symmetric(
-                        horizontal: 16,
-                        vertical: 12,
-                      ),
-                    ),
-                    hint: const Text('Seleccione un usuario'),
-                    items: _usersList.map((user) {
-                      return DropdownMenuItem<Users>(
-                        value: user,
-                        child: Text(user.user_Name ?? 'Usuario sin nombre'),
-                      );
-                    }).toList(),
-                    onChanged: (Users? user) {
-                      setState(() {
-                        _selectedUser = user;
-                      });
-                    },
-                  ),
-                  const SizedBox(height: 20),
-                  SizedBox(
-                    width: 200,
-                    child: ElevatedButton(
-                      onPressed: _registerUserBiometrics,
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.white,
-                        foregroundColor: Colors.blue.shade900,
-                        padding: const EdgeInsets.symmetric(vertical: 16),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        elevation: 0,
-                      ),
-                      child: const Text('Registrar Datos'),
-                    ),
-                  ),
-                  if (_isCapturing)
-                    Padding(
-                      padding: const EdgeInsets.only(top: 20),
-                      child: Column(
-                        children: [
-                          Text('Capturando imagen $_captureCount/3'),
-                          const SizedBox(height: 10),
-                          const CircularProgressIndicator(),
-                        ],
-                      ),
-                    ),
-                  if (_capturedImage != null && _facialPoints != null)
-                    Padding(
-                      padding: const EdgeInsets.only(top: 20),
-                      child: FutureBuilder<Uint8List>(
-                        future: _convertImageToDisplayFormat(_capturedImage!),
-                        builder: (context, snapshot) {
-                          if (snapshot.connectionState ==
-                                  ConnectionState.done &&
-                              snapshot.hasData) {
-                            return Stack(
-                              children: [
-                                Image.memory(
-                                  snapshot.data!,
-                                  height: 200, // Ajusta según necesites
-                                  width: 200, // Ajusta según necesites
-                                  fit: BoxFit.contain,
-                                ),
-                                CustomPaint(
-                                  painter: FacePointsPainter(_facialPoints!),
-                                ),
-                              ],
-                            );
-                          }
-                          return const CircularProgressIndicator();
-                        },
-                      ),
-                    ),
-                  if (_isLoading)
-                    const Padding(
-                      padding: EdgeInsets.only(top: 20),
-                      child: CircularProgressIndicator(),
-                    ),
+      body: Stack(
+        children: [
+          Container(
+            width: size.width,
+            height: size.height,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Colors.white,
+                  const Color.fromARGB(255, 96, 156, 217),
+                  const Color.fromARGB(255, 4, 134, 240),
                 ],
               ),
             ),
+            child: Center(
+              child: SafeArea(
+                child: SingleChildScrollView(
+                  padding: const EdgeInsets.symmetric(horizontal: 24),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Column(
+                        children: [
+                          InkWell(
+                            onTap: _captureAttendanceFace,
+                            child: Container(
+                              width: 150,
+                              height: 150,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                shape: BoxShape.circle,
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black.withOpacity(0.2),
+                                    spreadRadius: 2,
+                                    blurRadius: 5,
+                                    offset: const Offset(0, 3),
+                                  ),
+                                ],
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(15),
+                                child: Image.asset('assets/png/camara.png'),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          const Text(
+                            'Registrar Asistencia',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 20),
+                      DropdownButtonFormField<Users>(
+                        value: _selectedUser,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                            borderSide: BorderSide.none,
+                          ),
+                          contentPadding: const EdgeInsets.symmetric(
+                            horizontal: 16,
+                            vertical: 12,
+                          ),
+                        ),
+                        hint: const Text('Seleccione un usuario'),
+                        items: _usersList.map((user) {
+                          return DropdownMenuItem<Users>(
+                            value: user,
+                            child: Text(user.user_Name ?? 'Usuario sin nombre'),
+                          );
+                        }).toList(),
+                        onChanged: (Users? user) {
+                          setState(() {
+                            _selectedUser = user;
+                          });
+                        },
+                      ),
+                      const SizedBox(height: 20),
+                      SizedBox(
+                        width: 200,
+                        child: ElevatedButton(
+                          onPressed: _registerUserBiometrics,
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            foregroundColor: Colors.blue.shade900,
+                            padding: const EdgeInsets.symmetric(vertical: 16),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            elevation: 0,
+                          ),
+                          child: const Text('Registrar Datos'),
+                        ),
+                      ),
+                      if (_isCapturing)
+                        Padding(
+                          padding: const EdgeInsets.only(top: 20),
+                          child: Column(
+                            children: [
+                              Text('Capturando imagen $_captureCount/3'),
+                              const SizedBox(height: 10),
+                              const CircularProgressIndicator(),
+                            ],
+                          ),
+                        ),
+                      if (_isLoading)
+                        const Padding(
+                          padding: EdgeInsets.only(top: 20),
+                          child: CircularProgressIndicator(),
+                        ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
           ),
-        ),
+          if (_showWelcomeDialog)
+            Center(
+              child: Container(
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: const Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    if (_randomGifPath != null)
+                      Image.asset(
+                        _randomGifPath!,
+                        height: 150,
+                        width: 150,
+                        fit: BoxFit.contain,
+                      ),
+                    const SizedBox(height: 10),
+                    Text(
+                      '¡Bienvenido de vuelta $_welcomeUserName 😁!',
+                      style: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+        ],
       ),
     );
   }
